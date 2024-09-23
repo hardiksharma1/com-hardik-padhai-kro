05e044a860ac276a9487c340d62d538484124d95
hardiksharma1 <hardikbhardwaj15@gmail.com>
2024-09-24 02:48:12 +0530

    Removed Commented Code

--- src/main/java/com/hardik/padhai/karo/com_hardik_padhai_kro/bean/EmployeeBean.java
+++ src/main/java/com/hardik/padhai/karo/com_hardik_padhai_kro/bean/EmployeeBean.java
@@ -20,7 +20,6 @@ public class EmployeeBean {
 	public EmployeeBean(Integer empId, String empFirstName, String empLastName, String jobTitle, BigDecimal minSalary,
 			BigDecimal maxSalary) {
 	
-//		returnCommmonSetter(empId,empFirstName,empLastName,jobTitle,minSalary,maxSalary);
 		
 		this.empId = empId;
 		this.empFirstName = empFirstName;
@@ -37,17 +36,4 @@ public class EmployeeBean {
 		this(empId,empFirstName,empLastName,jobTitle,minSalary,maxSalary);
 		this.jobId = jobId;
 	}
-	
-//	private void returnCommmonSetter(Integer empId, String empFirstName, String empLastName, String jobTitle, BigDecimal minSalary,
-//			BigDecimal maxSalary) {
-//		
-//		this.empId = empId;
-//		this.empFirstName = empFirstName;
-//		this.empLastName = empLastName;
-//		this.jobTitle = jobTitle;
-//		this.minSalary = minSalary;
-//		this.maxSalary = maxSalary; 
-//	}
-//	
-	
 }
